7f778d663f12369baa38c5feb89fd6a2d6c92940
Sergio Castillo García <UO276436@uniovi.es>
2022-02-12 16:31:14 +0100

    SDI-2122-904-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2122904spring/Sdi2122904SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122904spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122904SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122904SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2122904spring/Sdi2122904SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2122904spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122904SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
