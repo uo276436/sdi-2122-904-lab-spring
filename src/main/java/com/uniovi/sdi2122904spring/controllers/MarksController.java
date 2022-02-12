85218650570e36cf6bbc8efe329296ae369d308b
Sergio Castillo García <UO276436@uniovi.es>
2022-02-12 17:47:32 +0100

    SDI-2021-904-2.3-Beans, servicios e inyección de dependencias.

--- src/main/java/com/uniovi/sdi2122904spring/controllers/MarksController.java
+++ src/main/java/com/uniovi/sdi2122904spring/controllers/MarksController.java
@@ -1,12 +1,18 @@
 package com.uniovi.sdi2122904spring.controllers;
 import com.uniovi.sdi2122904spring.entities.Mark;
+import com.uniovi.sdi2122904spring.services.MarksService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MarksController {
+
+    @Autowired //Inyectar el servicio
+    private MarksService marksService;
+
     @RequestMapping("/mark/list")
     public String getList() {
-        return "Getting List";
+        return marksService.getMarks().toString();
     }
     /*@RequestMapping(value ="/mark/add", method = RequestMethod.POST)
     public String setMark(@RequestParam String description, @RequestParam String score) {
@@ -14,9 +20,11 @@ public class MarksController {
     }*/
     @RequestMapping(value = "/mark/add", method = RequestMethod.POST)
     public String setMark(@ModelAttribute Mark mark) {
-        return "added: " + mark.getDescription()
+        /*return "added: " + mark.getDescription()
                 + " with score : " + mark.getScore()
-                + " id: " + mark.getId();
+                + " id: " + mark.getId();*/
+        marksService.addMark(mark);
+        return "OK";
     }
 
     /*
@@ -27,7 +35,13 @@ public class MarksController {
     */
     @RequestMapping("/mark/details/{id}") //http://localhost:8090/mark/details/4
     public String getDetail(@PathVariable Long id) {
-        return "Getting Details =>" + id;
+        //return "Getting Details =>" + id;
+        return marksService.getMark(id).toString();
+    }
+    @RequestMapping("/mark/delete/{id}")
+    public String deleteMark(@PathVariable Long id) {
+        marksService.deleteMark(id);
+        return "OK";
     }
 
 }
