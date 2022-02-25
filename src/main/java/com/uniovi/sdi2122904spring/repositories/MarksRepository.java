9ec9a0b928d341c5198c2ef87ff236f6f5d1b71a
Sergio Castillo García <UO276436@uniovi.es>
2022-02-25 18:30:22 +0100

    SDI-2122-904-4.5-SpringBoot-Búsqueda

--- src/main/java/com/uniovi/sdi2122904spring/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi2122904spring/repositories/MarksRepository.java
@@ -17,4 +17,8 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
     @Transactional
     @Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
     void updateResend(Boolean resend, Long id);
+    @Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1))")
+    List<Mark> searchByDescriptionAndName(String seachtext);
+    @Query("SELECT r FROM Mark r WHERE (LOWER(r.description) LIKE LOWER(?1) OR LOWER(r.user.name) LIKE LOWER(?1)) AND r.user = ?2")
+    List<Mark> searchByDescriptionNameAndUser(String seachtext, User user);
 }
