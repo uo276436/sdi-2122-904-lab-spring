7b53d71edd03242e046ccd0ca971d6c561202fc7
uo276436 <uo276436@uniovi.es>
2022-02-28 09:43:21 +0100

    SDI-2122-904-sesión4-Ejercicio-complementario2

--- src/main/java/com/uniovi/sdi2122904spring/controllers/ProfessorController.java
+++ src/main/java/com/uniovi/sdi2122904spring/controllers/ProfessorController.java
@@ -13,7 +13,8 @@ public class ProfessorController {
 
     @RequestMapping("/professor/details/{dni}")
     public String getDetail(@PathVariable String dni) {
-        return professorService.getProfessor(dni).toString();
+        //return professorService.getProfessor(dni).toString();
+        return "details "+dni;
     }
     @RequestMapping(value = "/professor/add", method = RequestMethod.POST)
     public String setProfessor(@ModelAttribute Professor professor) {
@@ -21,17 +22,21 @@ public class ProfessorController {
         return "added: " + professor.getDni()
                 + " with dni : " + professor.getDepartment();
     }
+    @RequestMapping(value = "/professor/add/{dni}")
+    public String setProfessor(@PathVariable String dni) {
+        return "add "+dni;
+    }
     @RequestMapping("/professor/list")
     public String getList() {
         return professorService.getProfessors().toString();
     }
     @RequestMapping(value = "/professor/edit/{dni}")
-    public String setProfessor(@PathVariable String dni) {
+    public String editProfessor(@PathVariable String dni) {
         return "editing "+dni;
     }
     @RequestMapping(value = "/professor/delete/{dni}")
     public String deleteProfessor(@PathVariable String dni) {
-        professorService.deleteMark(dni);
+        //professorService.deleteMark(dni);
         return "remove "+dni;
     }
 }
