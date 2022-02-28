fa3c47456ef5bc1bf8f07589619e933751cd6592
Sergio Castillo García <UO276436@uniovi.es>
2022-02-28 19:30:47 +0100

    SDI-2122-904-5.4-Casos Uso VistaHome.

--- src/test/java/com/uniovi/sdi2122904spring/Sdi2122904SpringApplicationTests.java
+++ src/test/java/com/uniovi/sdi2122904spring/Sdi2122904SpringApplicationTests.java
@@ -1,9 +1,13 @@
 package com.uniovi.sdi2122904spring;
 
+import com.uniovi.sdi2122904spring.pageobjects.*;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.*;
+
+import java.util.List;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -46,5 +50,38 @@ class Sdi2122904SpringApplicationTests {
     static public void end() {
         //Cerramos el navegador al finalizar las pruebas
         driver.quit();
+    }
+
+    @Test
+    @Order(1)
+    void PR01A() {
+        PO_HomeView.checkWelcomeToPage(driver, PO_Properties.getSPANISH());
+    }
+    @Test
+    @Order(2)
+    void PR01B() {
+        List<WebElement> welcomeMessageElement = PO_HomeView.getWelcomeMessageText(driver,
+                PO_Properties.getSPANISH());
+        Assertions.assertEquals(welcomeMessageElement.get(0).getText(),
+                PO_HomeView.getP().getString("welcome.message", PO_Properties.getSPANISH()));
+    }
+    //PR02. Opción de navegación. Pinchar en el enlace Registro en la página home
+    @Test
+    @Order(3)
+    public void PR02() {
+        PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+    }
+    //PR03. Opción de navegación. Pinchar en el enlace Identifícate en la página home
+    @Test
+    @Order(4)
+    public void PR03() {
+        PO_HomeView.clickOption(driver, "login", "class", "btn btn-primary");
+    }
+    //PR04. Opción de navegación. Cambio de idioma de Español a Inglés y vuelta a Español
+    @Test
+    @Order(5)
+    public void PR04() {
+        PO_HomeView.checkChangeLanguage(driver, "btnSpanish", "btnEnglish",
+                PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
     }
 }
